2a412e54a21e2a84d2ed9fe45b4a53ee2835e2e7
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-02-28 20:53:55 +0100

    Realisation de la SCROLL UP

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,3 +49,12 @@ const swiperPopular = new Swiper(".popular__swiper", {
     },
   },
 });
+
+/*======== SCROLL UP =======*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
